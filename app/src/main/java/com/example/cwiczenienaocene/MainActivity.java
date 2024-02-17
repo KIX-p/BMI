3b0d12fa27032e35715450a08f4387635d886030
Pawel Kikh <pavlokiht2005@gmail.com>
2024-02-17 12:21:04 +0100

    BMI app

--- app/src/main/java/com/example/cwiczenienaocene/MainActivity.java
+++ app/src/main/java/com/example/cwiczenienaocene/MainActivity.java
@@ -1,5 +1,7 @@
 package com.example.cwiczenienaocene;
 
+import static java.lang.Math.round;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
@@ -19,7 +21,6 @@ public class MainActivity extends AppCompatActivity {
 
         Button button1 = (Button) findViewById(R.id.button1);
 
-
         button1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -27,9 +28,21 @@ public class MainActivity extends AppCompatActivity {
                 EditText polewaga = findViewById(R.id.polewaga);
                 TextView wynik = findViewById(R.id.wynik);
                 int wzrost = Integer.parseInt(polewzrost.getText().toString());
-                int waga = Integer.parseInt(polewzrost.getText().toString());
-                int bmi = waga/(wzrost/100);
-                wynik.setText(getText(bmi));
+                int waga = Integer.parseInt(polewaga.getText().toString());
+                double bmi = waga/ ((wzrost / 100.0) * (wzrost / 100.0));
+                bmi = round(bmi * 10.0) / 10.0;
+                wynik.setText(String.valueOf(bmi));
+
+
+                if(bmi < 18.5) {
+                    Toast.makeText(getApplicationContext(), "Niedowaga", Toast.LENGTH_LONG).show();
+                } else if (bmi >= 18.5 && bmi < 24.9) {
+                    Toast.makeText(getApplicationContext(), "Waga prawidłowa", Toast.LENGTH_LONG).show();
+                } else if (bmi >= 25 && bmi < 29.9) {
+                    Toast.makeText(getApplicationContext(), "Nadwaga", Toast.LENGTH_LONG).show();
+                } else if (bmi >= 30) {
+                    Toast.makeText(getApplicationContext(), "Otyłość", Toast.LENGTH_LONG).show();
+                }
             }
         });
     }
